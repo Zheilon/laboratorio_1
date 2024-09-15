bddc8ec8d23645a2bdfee65580ec43e0965bd1cf
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-09-15 13:12:18 -0500

    Software Dias de Retraso: Nuevas variables. -V 0.0.1

---
+++ calcular_dias_retraso/devOne.py
@@ -0,0 +1,17 @@
+#En la variable diasRetraso se almacena el valor
+#ingresado por consola.
+diasRetraso = int(input("Ingrese Días de retraso: "))
+
+#En la variable diasAsignados se almacena el valor
+#ingresado por consola.
+diasAsignados = int(input("Ingrese Días Asignados: "))
+
+#En la variable se almacena el resultado de la 
+#formula para hallar el porcentaje de retraso - 
+#(dias de retraso / dias asignados) * 100.
+porcentajeRetraso = (diasRetraso / diasAsignados) * 100
+
+#Por medio de este print se imprime por consola
+#el resultado de la variable porcentaje: 
+#porcentajeRetraso.
+print(f"Porcentaje de retraso: {int(porcentajeRetraso)} %")
