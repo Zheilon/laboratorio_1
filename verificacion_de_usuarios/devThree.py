4f300b28d04ab1cffe9cef5f1022e789bc06f594
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-09-15 13:55:56 -0500

    Software Verification Users: If Structure / New Vars -V 0.0.1

---
+++ verificacion_de_usuarios/devThree.py
@@ -0,0 +1,33 @@
+print(" ---------> Sistema de control de acceso <---------\n")
+
+#La variable nivelAcceso almacena el valor que se ingresó.
+#por la consola.
+nivelAcceso = int(input("Ingrese su nivel de acceso: "))
+print()
+
+#La variable consulta almacena el valor que se ingresó.
+#por la consola.
+consulta = int(input('''Indique en días el último cambio que
+tuvo la contraseña de su targeta: '''))
+print()
+
+#La variable actividadTargeta almenacena un resultado de
+#dos opciones disponibles: True = t / False = f. 
+actividadTargeta = str(input("¿Targeta de identificación ACTIVA? True = t / False = f: "))
+print()
+
+#En esta estructura if, else-if se evalua la condición del nivel de acceso,
+#el cambio de contraseña en los último 30 días, la actividad de targeta
+#el cual acompañando la variable actividadTargeta esta el método lower()
+#que devuelve un str de minusculas.
+if nivelAcceso == 5 and consulta <= 30 and actividadTargeta.lower() == 't':
+    print("Acceso Concedido!")
+
+elif nivelAcceso == 0 and consulta > 30 and actividadTargeta.lower() == 'f':
+    print("Acceso Denegado!")
+
+#En este else, utilizado para cuando el usuario no halla ingresado alguno
+#de los campos correctamente. 
+else:
+    print("Ingrese indicación correcta!")
+
